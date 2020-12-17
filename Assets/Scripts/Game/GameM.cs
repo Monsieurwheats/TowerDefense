bea8bcf4f8685868c093e18361c737de71643fb8
Olivier Demers <olidem123@gmail.com>
2020-12-17 14:04:02 -0500

    Better UI

--- Assets/Scripts/Game/GameM.cs
+++ Assets/Scripts/Game/GameM.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameM : MonoBehaviour
 {
@@ -8,23 +10,46 @@ public class GameM : MonoBehaviour
     public AudioSource GamePlaySound;
     public AudioClip wavestart;
 
+    [SerializeField] private Image fastButton;
+
+    private bool _isFast;
+
+    private bool IsFast
+    {
+        get => _isFast;
+        set
+        {
+            _isFast = value;
+            fastButton.color = _isFast ? Color.yellow : Color.gray;
+        }
+        
+    }
+
     public void Start()
     {
         Game.GameManager = this;
+        IsFast = false;
     }
 
     
 
     public void PlayerWave()
     {
-        if(Game.WaveSpawner.playing == false && Game.WaveSpawner.enabled == true) // tempo cond maybe
-        {
-            UIsound.clip = wavestart;
-            UIsound.Play();
-            Game.WaveSpawner.StartWave();
-        }
-  
+        if (Game.WaveSpawner.playing || !Game.WaveSpawner.enabled) return;
+        UIsound.clip = wavestart;
+        UIsound.Play();
+        Game.WaveSpawner.StartWave();
+    }
+
+    public void Fast()
+    {
+        IsFast = !IsFast;
+        Time.timeScale = IsFast ? 2 : 1;
+    }
 
 
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
