5022121f57a425d5df01d427c0a2258cac123d02
Olivier Demers <olidem123@gmail.com>
2020-12-16 21:49:09 -0500

    Removed unnecessary file

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -64,5 +64,26 @@ public class WaveSpawner : MonoBehaviour
         x.Level = enemy.level;
         EAlive++;
     }
+    
+    
+    [System.Serializable]
+    public class Waves
+    {
+        //public float count;
+        public float rate;
+        public Enemy[] Enemies = new Enemy[20];
+        //public Minions[] e = new Minions[1];
+
+
+
+
+    }
+
+    [System.Serializable]
+    public struct Enemy
+    {
+        public Minions minions;
+        public int level;
+    }
 
 }
